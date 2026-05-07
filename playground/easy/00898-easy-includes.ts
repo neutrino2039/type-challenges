1a80a3fafe311bc8d873a4fd2c0f613551de4632
Aman Maharjan <neutrino2039@gmail.com>
2026-05-07 23:17:00 +0200

    implement Includes

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -18,7 +18,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly any[], U> =
+  T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true
+      ? true
+      : Includes<Rest, U>
+    : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
